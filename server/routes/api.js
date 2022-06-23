a698fcd119e8ca3ef913474b092a9d2683fef8e3
Phuong Tran <ptranny@gmail.com>
2022-06-23 20:58:37 +1200

    Expose POST /api/v1/spotify seAPI

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,30 @@
 const express = require('express')
+const request = require('superagent')
 require('dotenv').config()
 
 const router = express.Router()
+
+router.get('/spotify', async (req, res) => {
+  try {
+    const tokenRequest = request
+      .post('https://accounts.spotify.com/api/token')
+      .set('Authorization', `Basic ${process.env.SPOTIFY_AUTH}`)
+      .type('form')
+      .send({ grant_type: 'client_credentials' })
+
+    const tokenResponse = await tokenRequest
+    const token = tokenResponse.body.access_token
+
+    const musicRequest = request
+      .get('https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy')
+      .set('Authorization', `Bearer ${token}`)
+
+    const musicResponse = await musicRequest
+    res.json(musicResponse.body)
+  } catch (error) {
+    console.error(error.message)
+    res.sendStatus(500)
+  }
+})
 
 module.exports = router
