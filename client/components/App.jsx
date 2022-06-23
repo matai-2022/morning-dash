e7ff2fa4da8b4a81c46d5847c5eddccf5d0a9ab1
Phuong Tran <ptranny@gmail.com>
2022-06-23 20:57:50 +1200

    Fix broken import links

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useReducer } from 'react'
 
-import * as api from './api'
+import * as api from '../api'
 
 import Dashboard from './Dashboard.jsx'
 import Widget from './Widget.jsx'
