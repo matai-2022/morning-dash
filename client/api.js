b59b2bbd9f02cfb9f650942c23879da6deede353
Tony <tonykha17@hotmail.com>
2022-06-24 11:07:15 +1200

    done 3 client side api gets

--- client/api.js
+++ client/api.js
@@ -1,1 +1,28 @@
 import request from 'superagent'
+
+const spotifyUrl = '/api/v1/spotify'
+const newsUrl = '/api/v1/news'
+const kanyeUrl = 'api/v1/kanye'
+
+//get spotify function
+
+const getSpotify = async () => {
+  const res = await request.get(spotifyUrl)
+  return res.body
+}
+
+//get news function
+
+const getNews = async () => {
+  const res = await request.get(newsUrl)
+  return res.body
+}
+
+//get kanye function
+
+const getKanye = async () => {
+  const res = await request.get(kanyeUrl)
+  return res.body
+}
+
+export { getSpotify, getNews, getKanye }
--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Widget from './Widget'
+import * as api from '../api.js'
 
-function Dashboard () {
+//widgetname widgetdata
 
+function Dashboard() {
+  useEffect(async () => {
+    try {
+      // const spotify = await api.getSpotify()
+      const news = await api.getNews()
+      const kanye = await api.getKanye()
+      console.log('hi')
+    } catch (error) {
+      console.error(error)
+    }
+  }, [])
+
+  return (
+    <>
+      <Widget widgetName={} widgetData={kanye}/>
+    </>
+  )
 }
 
 export default Dashboard
